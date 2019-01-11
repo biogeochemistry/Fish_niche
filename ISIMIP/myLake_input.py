16503966dcd73d468d90ce275a4ae29afee3b994
JulienBellavance <julien.bellavance.1@ulaval.ca>
2019-01-11 11:20:35 -0500

    Creation of ISIMIP directory

--- ISIMIP/myLake_input.py
+++ ISIMIP/myLake_input.py
@@ -1,11 +1,12 @@
-from mylake_init_ISIMIP import *
 import numpy as np
 import pandas as pd
-import h5py
 import datetime
 import os
 import shutil
 import bz2
 import math
 import sys
+import netCDF4
 
+def myLake_input():
+    pass
