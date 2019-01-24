b727765dfc8a1bda6b2c910d43d0b6c1ad6789b4
MarianneCote <38430290+MarianneCote@users.noreply.github.com>
2019-01-24 13:40:03 -0500

    Create plot_multiple_regression.R
    3D visualization of multiple linear regression(multiple predictors) + visualization of partial regression and test of residuals distribution.

---
+++ output_analysis_scripts/plot_multiple_regression.R
@@ -0,0 +1,67 @@
+library(plotly)
+library(reshape2)
+
+#load data
+
+my_df <- dontest
+my_df$Lnsod
+colnames(my_df)[24] <- 'LnSOD'
+colnames(my_df)[5] <- 'MAX'
+colnames(my_df)[16] <- 'LnArea'
+SOD_lm <- lm(LnSOD ~ MAX + LnArea,data = my_df)
+summary(SOD_lm)
+#Graph Resolution (more important for more complex shapes)
+graph_reso <- 0.05
+
+#Setup Axis
+axis_x <- seq(min(my_df$MAX), max(my_df$MAX), by = graph_reso)
+axis_y <- seq(min(my_df$LnArea), max(my_df$LnArea), by = graph_reso)
+
+#Sample points
+SOD_lm_surface <- expand.grid(MAX = axis_x,LnArea = axis_y,KEEP.OUT.ATTRS = F)
+SOD_lm_surface$LnSOD <- predict.lm(SOD_lm, newdata = SOD_lm_surface)
+SOD_lm_surface <- acast(SOD_lm_surface, LnArea ~ MAX, value.var = "LnSOD") #y ~ x
+
+hcolors=c("black")
+SOD_plot <- plot_ly(my_df, 
+                     x = ~MAX, 
+                     y = ~LnArea, 
+                     z = ~LnSOD,
+                     text = "Species", 
+                     type = "scatter3d", 
+                     mode = "markers",
+                     marker = list(color = hcolors))
+
+SOD_plot <- add_trace(p = SOD_plot,
+                       z = SOD_lm_surface,
+                       x = axis_x,
+                       y = axis_y,
+                       type = "surface",
+                       opacity = 0.9,
+                       color="black")%>%
+  layout(scene = list(xaxis = list(title = 'Max'),
+                    yaxis = list(title = 'Ln(Area)'),
+                    zaxis = list(title = 'Ln(k_SOD)')),
+       annotations = list(
+         x = 1.13,
+         y = 1.05,
+         text = 'test',
+         xref = 'paper',
+         yref = 'paper',
+         showarrow = FALSE
+       ))
+
+
+SOD_plot
+
+
+fit4 <-lm(log(dontest$k_SOD) ~  dontest$Max + log(dontest$Area))
+library(car)
+library(MASS)
+avPlots(fit4)
+summary(fit4)
+hist(fit4$residuals, breaks = 4)
+qqnorm(fit4$residuals)
+qqline(fit4$residuals)
+
+shapiro.test(fit4$residuals)
