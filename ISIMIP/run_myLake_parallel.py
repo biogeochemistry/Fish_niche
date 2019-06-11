7421204c736e48798d7651cce48d66fddea1ad43
JulienBellavance <julien.bellavance.1@ulaval.ca>
2019-06-11 15:39:40 -0400

    Small additions to parrallel module

--- ISIMIP/run_myLake_parallel.py
+++ ISIMIP/run_myLake_parallel.py
@@ -6,3 +6,10 @@ import myLake_post
 Module that allows running multiple simulations in parallel.
 """
 
+def loop_through_lakes():
+    """
+    Loops through all lakes and runs
+    :return:
+    """
+
+    pass
